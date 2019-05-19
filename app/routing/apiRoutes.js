58b8f7753968b8173ca7ebadb019f9495fecf4a8
Chris Nyberg <chrisn83@gmail.com>
2019-05-19 13:37:23 -0400

    added validate form code

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,10 @@
+const path = require("path");
+// const friends = path.json(__dirname, "app/data/friendsList.json")
+
+module.exports = app => {
+
+	// app.get("/api/friends", (req, res) => {
+	// 	return app.get(friends)
+    // });
+    
+};
