49104d152a1dcbc41ad1e63c5b9366a6aa108e0d
Chris Nyberg <chrisn83@gmail.com>
2019-05-20 21:10:44 -0400

    started closest match function linked to apiroutes

---
+++ app/data/friends.js
@@ -0,0 +1,13 @@
+
+
+module.exports = (user, data) => {
+function closestMatch () {
+    for(i=0; i < data.length; i++) {
+        data[i];
+        for(n=0; n < data.answers.length; n++) {
+            user.answers[n] - data.answers[n];
+            console.log(n)
+        }
+    }
+}
+}
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const friendsList = require("../data/friendsList.json");
-
+const closestMatch = require("../data/friends.js");
 module.exports = app => {
   app.get("/api/friends", (req, res) => {
     res.json(friendsList);
@@ -9,7 +9,9 @@ module.exports = app => {
   app.post("/api/friends", (req, res) => {
     const friendData = req.body;
 
+    closestMatch(friendData, friendsList);
     friendsList.push(friendData);
+    
     res.end(); //send closest match function
   });
 };
